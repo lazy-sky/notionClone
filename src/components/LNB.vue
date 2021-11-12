ef41eb1684e61e0830937b304de443e1ea7a20ae
lazy-sky <1cielo@naver.com>
2021-11-12 21:07:44 +0900

    Render workspace titles

--- src/components/LNB.vue
+++ src/components/LNB.vue
@@ -1,5 +1,23 @@
 <template>
   <nav style="width: 240px">
-    LNB!
+    <div class="header"></div>
+    <ul>
+      <li
+        v-for="workspace in workspaces"
+        :key="workspace.id">
+        {{ workspace.title || '제목 없음' }}
+      </li>
+    </ul>
+    <div class="actions"></div>
   </nav>
 </template>
+
+<script>
+export default {
+  computed: {
+    workspaces() {
+      return this.$store.state.workspace.workspaces
+    }
+  }
+}
+</script>
