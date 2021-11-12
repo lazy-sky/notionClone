80ca0d99d0643e8f465de1a34cc875cfff406029
lazy-sky <1cielo@naver.com>
2021-11-12 21:15:20 +0900

    Separate WorkspaceItem from LNB

--- src/components/LNB.vue
+++ src/components/LNB.vue
@@ -2,18 +2,23 @@
   <nav style="width: 240px">
     <div class="header"></div>
     <ul>
-      <li
+      <WorkspaceItem
         v-for="workspace in workspaces"
-        :key="workspace.id">
-        {{ workspace.title || '제목 없음' }}
-      </li>
+        :key="workspace.id"
+        :workspace="workspace" />
     </ul>
     <div class="actions"></div>
   </nav>
 </template>
 
 <script>
+import WorkspaceItem from '~/components/WorkspaceItem'
+
 export default {
+  components: {
+    WorkspaceItem
+  },
+
   computed: {
     workspaces() {
       return this.$store.state.workspace.workspaces
---
+++ src/components/WorkspaceItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>
+    {{ workspace.title || '제목 없음' }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    workspace: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
