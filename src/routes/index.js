33623e70127125a46ef572a6fe68bab306d63f0a
lazy-sky <1cielo@naver.com>
2021-11-12 20:28:53 +0900

    feat: Rename Home to Workspace

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from './Home'
+import Workspace from './Workspace'
 import About from './About'
 
 export default createRouter({
@@ -8,7 +8,12 @@ export default createRouter({
   routes: [
     {
       path: '/',
-      component: Home
+      component: Workspace,
+      children: {
+        name: 'Workspace',
+        path: 'workspaces/:id',
+        component: Workspace
+      }
     },
     {
       path: '/about',
