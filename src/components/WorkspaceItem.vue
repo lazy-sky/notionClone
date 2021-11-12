bce02c46371ea8151d21465d85c94ee81da0482c
lazy-sky <1cielo@naver.com>
2021-11-12 21:27:41 +0900

    buttons

--- src/components/WorkspaceItem.vue
+++ src/components/WorkspaceItem.vue
@@ -1,6 +1,28 @@
 <template>
   <li>
-    {{ workspace.title || '제목 없음' }}
+    <div
+      class="title"
+      @click="$router.push({
+        name: 'Workspace',
+        params: {
+          id: workspace.id
+        }
+      })">
+      <span class="material-icons">
+        play_arrow
+      </span>
+      <span class="text">
+        {{ workspace.title || '제목 없음' }}
+      </span>
+      <div class="actions">
+        <span class="material-icons">
+          add
+        </span>
+        <span class="material-icons">
+          delete
+        </span>
+      </div>
+    </div>
   </li>
 </template>
 
