467d3e0805803394cd898655aed261922bea8e0b
lazy-sky <1cielo@naver.com>
2021-11-15 20:13:20 +0900

    style: Workplace Item

--- src/components/WorkspaceItem.vue
+++ src/components/WorkspaceItem.vue
@@ -3,6 +3,7 @@
     <div
       :style="{ paddingLeft: `${14 * depth}px` }"
       class="title"
+      :class="{ active: $route.params.id === workspace.id }"
       @click="$router.push({
         name: 'Workspace',
         params: {
@@ -71,3 +72,72 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+li {
+  cursor: pointer;
+  user-select: none;
+
+  .title {
+    display: flex;
+    align-items: center;
+    white-space: nowrap;
+    height: 30px;
+    padding: 0 14px;
+    position: relative;
+    color: rgba($color-font, .7);
+
+    &:hover {
+      background-color: $color-background--hover1;
+      padding-right: 4px;
+
+      .actions {
+        display: flex;
+      }
+    }
+
+    &.active {
+      .text {
+        font-weight: 700;
+        color: rgba($color-font, .8);
+      }
+    }
+
+    .material-icons {
+      font-size: 18px;
+      color: $color-icon;
+      margin-right: 4px;
+
+      &:hover {
+        background-color: $color-background--hover2;
+      }
+
+      &.active {
+        transform: rotate(90deg);
+      }
+    }
+
+    .text {
+      flex-grow: 1;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+    }
+
+    .actions {
+      display: none;
+      align-items: center;
+    }
+  }
+  
+  .no-children {
+    color: rgba($color-font, .35);
+    height: 30px;
+    display: flex;
+    align-items: center;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+}
+</style>
