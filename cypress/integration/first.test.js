4e53ab215746b3ce2fbc40aef5be9b93531a5cac
lazy-sky <1cielo@naver.com>
2021-11-17 20:40:33 +0900

    cypress practice

--- cypress/integration/first.test.js
+++ cypress/integration/first.test.js
@@ -1,5 +1,25 @@
 describe('', () => {
-  it('', () => {
+  it('visit test', () => {
+    // 근데 오류남
     cy.visit('/')
+  })
+
+  it('nested page visit', () => {
+    cy.get('nav ul li:first-child span.material-icons')
+      .contains('play_arrow')
+      .click()
+    
+    cy.wait(1000)
+    
+    cy.get('nav ul li:nth-child(1) > ul li:nth-child(1) .text')
+      .contains('Hello world!')
+      .click()
+  })
+
+  it('', () => {
+    cy.get('.page__container .title[contenteditable]')
+      .clear()
+      .type('반갑습니다')
+      .blur()
   })
 })
