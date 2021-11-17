c980292e62c854a0a297cb5d9d0d559140fc5498
lazy-sky <1cielo@naver.com>
2021-11-17 19:38:03 +0900

    open graph protocol

--- functions/render.js
+++ functions/render.js
@@ -1,17 +1,54 @@
-exports.handler = async function () {
+const axios = require('axios')
+
+exports.handler = async function (event) {
+  const { path }= event
+  const params = path.split('/').filter(p => p)
+  const og = {
+    title: 'Lazysky Notion',
+    description: '노션 클론 프로젝트입니다!',
+    image: '',
+  }
+  
+  if (params[0] === 'workspace' && params[1]) {
+    try {
+      const { data } = await axios({
+        url: `https://asia-northeast3-heropy-api.cloudfunctions.net/api/notion/workspaces/${params[1]}`,
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+          'apikey': 'FcKdtJs202110',
+          'username': 'abcde'
+        }
+      })
+  
+      const { title, content, poster } = data
+      og.title = title
+      og.description = content
+      og.image = poster
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return {
     statusCode: 200,
     headers: {
       'content-type': 'text/html; charset=UTF-8'
     },
-    body: 
+    body: /*HTML*/
     `<!DOCTYPE html>
     <html lang="ko">
     <head>
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Notion Clone!!</title>
+      <meta property="og:type" content="website">
+      <meta property="og:site_name" content="Notion Clone">
+      <meta property="og:title" content="${og.title}">
+      <meta property="og:description" content="${og.description}">
+      <meta property="og:image" content="${og.image}">
+      <meta property="og:url" content="https://google.com/workspaces/${path}">
+      <title>${og.title}</title>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css" />
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <link rel="preconnect" href="https://fonts.googleapis.com">
